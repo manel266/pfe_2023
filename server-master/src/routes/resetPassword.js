116a38a7949c10d7c15ff81883dd265e28f0dc03
manelhkimi26 <manelhkimi26@gmail.com>
2023-05-20 09:32:34 +0100

    f1

--- server-master/src/routes/resetPassword.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userController = require("../controller/resetPassword");
-
-router.put("/password/:email", userController.updatePassword);
-
-module.exports = router;
